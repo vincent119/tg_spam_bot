543547ba7e3d03ba0f752cdceee28246a84b1f3c
vincent <vincent119@gmail.com>
2026-07-21 10:36:59 +0800

    chore(migrations): remove obsolete detection tables migration
    - Delete down migration for detection tables schema
    - Clean up deprecated database structure (trusted_members, enforcement_actions, violations, detection_events, processed_updates)
    - Simplify migration state management by removing unused rollback

--- migrations/20260717000100_create_detection_tables.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS trusted_members;
-DROP TABLE IF EXISTS enforcement_actions;
-DROP TABLE IF EXISTS violations;
-DROP TABLE IF EXISTS detection_events;
-DROP TABLE IF EXISTS processed_updates;
